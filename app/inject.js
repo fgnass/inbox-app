63a687146753803c0648b24607401fb977035434
Felix Gnass <fgnass@gmail.com>
2016-01-31 00:21:17 +0100

    re-inject upon reload

--- app/inject.js
+++ app/inject.js
@@ -1,14 +1,18 @@
 var fs = require('fs');
 var path = require('path');
+var URL = require('url');
 
 var dir = path.resolve(__dirname, '..');
 
 module.exports = function(win) {
-  win.once('page-title-updated', function() {
-    var wc = win.webContents;
-    wc.insertCSS(fs.readFileSync(dir + '/web/custom.css', 'utf8'));
-    wc.executeJavaScript('module.paths.push("' + dir + '/node_modules");');
-    wc.executeJavaScript('module.paths.push("' + dir + '/web");');
-    wc.executeJavaScript('require("inject");');
+  var wc = win.webContents;
+  wc.on('did-navigate', function(ev, url) {
+    var host = URL.parse(url).host;
+    if (host == 'inbox.google.com') {
+      wc.insertCSS(fs.readFileSync(dir + '/web/custom.css', 'utf8'));
+      wc.executeJavaScript('module.paths.push("' + dir + '/node_modules");');
+      wc.executeJavaScript('module.paths.push("' + dir + '/web");');
+      wc.executeJavaScript('require("inject");');
+    }
   });
 };
