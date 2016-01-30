22980d4d2c0c410e0ee8ec540902f0a70bfd9c96
Felix Gnass <fgnass@gmail.com>
2016-01-31 00:59:31 +0100

    only open one window per account

--- app/inbox.js
+++ app/inbox.js
@@ -4,7 +4,35 @@ var BrowserWindow = electron.BrowserWindow;
 var inject = require('./inject');
 var windows = require('./windows');
 
+function getUserId(url) {
+  // The `authuser` parameter is present when switching profiles
+  var m = url.match(/authuser=(\d)/);
+
+  // ... otherwise the URLs look like this: `/u/<id>`
+  if (!m) m = url.match(/\/u\/(\d)/);
+
+  // ... or just `/` for the default user
+  return m ? parseFloat(m[1]) : 0;
+}
+
+function getUserWindow(id) {
+  var all = BrowserWindow.getAllWindows();
+  for (var i = 0; i < all.length; i++) {
+    var win = all[i];
+    var url = win.webContents.getURL();
+    if (getUserId(url) == id) return win;
+  }
+}
+
 exports.open = function(url) {
+  // look for an existing window
+  var id = getUserId(url);
+  var win = getUserWindow(id);
+  if (win) {
+    win.show();
+    return win;
+  }
+
   var win = new BrowserWindow({
     width: 1024,
     height: 768,
